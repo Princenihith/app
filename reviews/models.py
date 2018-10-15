2316f620a4d229d28991b9287f22ab76a47d214b
princenihith <princenihith@gmail.com>
2018-10-15 13:57:01 +0530

    trying

--- reviews/models.py
+++ reviews/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from accounts.models import User
-import numpy as np
+# import numpy as np
 
 
 class Wine(models.Model):
     name = models.CharField(max_length=200)
     photo = models.ImageField(upload_to = 'product_image', null=True, blank=True)
     
-    def average_rating(self):
-        all_ratings = map(lambda x: x.rating, self.review_set.all())
-        return np.mean(all_ratings)
+    # def average_rating(self):
+    #     all_ratings = map(lambda x: x.rating, self.review_set.all())
+    #     return np.mean(all_ratings)
         
     def __unicode__(self):
         return self.name
