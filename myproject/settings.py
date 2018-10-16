ad4b9e4fda6b32ae03adecb92179ef0bffc58c69
princenihith <princenihith@gmail.com>
2018-10-16 13:28:05 +0530

    done

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zxs+=dn1nlctk1sa47t&!kbe2hx(^k_at(fr81mlu4yz&yjwt4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ 'secret-ravine-63793.herokuapp.com',]
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'home',
     'reviews',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
