1288f1b8ff4eef1cab1c8596f48b2425721cff8e
princenihith <princenihith@gmail.com>
2018-10-17 21:39:24 +0530

    email

--- myproject/settings.py
+++ myproject/settings.py
@@ -43,6 +43,11 @@ INSTALLED_APPS = [
     # 'bootstrap3',
 ]
 
+AUTHENTICATION_BACKENDS = [
+
+    'accounts.authentication.EmailAuthbackend',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
