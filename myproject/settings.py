b275b6693210f36d1173d5cdc3fc381d9c826f14
princenihith <princenihith@gmail.com>
2018-10-17 20:13:44 +0530

    email

--- myproject/settings.py
+++ myproject/settings.py
@@ -149,3 +149,11 @@ LOGIN_EXEMPT_URLS = [
    r'^accounts/reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
    r'^accounts/reset-password/complete/$',
     ]
+
+
+EMAIL_BACKEND = ‘django.core.mail.backends.smtp.EmailBackend’
+EMAIL_HOST = ‘smtp.gmail.com’
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ‘princenihith.1@gmail.com’
+EMAIL_HOST_PASSWORD = ‘9848033221123456..’
