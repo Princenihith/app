9c0e1dc9bef3fca01515e808bd7cd25476e6f203
princenihith <princenihith@gmail.com>
2018-10-18 14:13:00 +0530

    login redirect

--- myproject/settings.py
+++ myproject/settings.py
@@ -137,6 +137,10 @@ STATICFILES_DIRS = [
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'myproject/media/')
+
+
 LOGIN_REDIRECT_URL = '/home/'
 LOGIN_URL = '/accounts/login/'
 
