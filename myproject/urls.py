0178daef07b4c9f70a32b59c251775a1f0a37593
princenihith <princenihith@gmail.com>
2018-10-18 21:53:20 +0530

    media

--- myproject/urls.py
+++ myproject/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('reviews/', include('reviews.urls', namespace='reviews')),
 
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
