e6769bd8a74c533f983f27f008b2cf92caacd04d
princenihith <princenihith@gmail.com>
2018-10-18 21:57:39 +0530

    media

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from tutorial.views import index
+from django.conf import settings
+from django.conf.urls import url
 from myproject.views import login_redirect
 from django.conf.urls.static import static
 urlpatterns = [
