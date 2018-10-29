77e2a6fab6d4bc22b17b07f2371e1ffda9c8448f
princenihith <princenihith@gmail.com>
2018-10-29 22:49:56 +0530

    landing

--- myproject/settings.py
+++ myproject/settings.py
@@ -129,19 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-# STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, 'static'),
-#     ]
-STATICFILES_DIRS =( os.path.join(STATIC_ROOT, 'css/'),
-                    os.path.join(STATIC_ROOT, 'javascript/'),
-                    os.path.join(STATIC_ROOT, 'images/')
-                  )
-
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'myproject/media/')
